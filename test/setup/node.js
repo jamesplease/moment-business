26a39e977232b6b987a80528b25f17bc4589f884
Jmeas <jellyes2@gmail.com>
2015-02-14 01:12:35 -0500

    Convert tests to ES6

--- test/setup/node.js
+++ test/setup/node.js
@@ -1,5 +1,3 @@
-var setup = require('./setup');
-var config = require('../../config');
 global._ = require('underscore');
 require('../fixtures/work-diff/sunday');
 require('../fixtures/work-diff/monday');
@@ -11,9 +9,9 @@ require('../fixtures/work-diff/saturday');
 require('../fixtures/modify/add');
 require('../fixtures/modify/subtract');
 
+require("6to5/register");
 global.moment = require('moment');
-require('../../src/' + config.entryFileName);
 global.chai = require('chai');
 global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
-setup();
+require('./setup')();
